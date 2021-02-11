98f4f77534b662405f7c05b22505f2f783552e33
dongsiku <dongsiku@users.noreply.github.com>
2021-02-11 14:11:40 +0000

    deploy: e1c0181412710a076e3442d44408ce58de2d2c8b

---
+++ js/create_options.js
@@ -0,0 +1,112 @@
+const START_STOP_BUTTON = document.getElementById('start-stop-btn');
+const B4_TIME_BUTTON = document.getElementById('b4-time-btn');
+const MX_TIME_BUTTON = document.getElementById('mx-time-btn');
+const CUSTOM_TIME_BUTTON = document.getElementById('custom-time-btn');
+const LOG_WINDOW = document.getElementById('log-window');
+const selectTags = [
+  [
+    document.getElementById('first-min'),
+    document.getElementById('first-sec'),
+  ],
+  [
+    document.getElementById('second-min'),
+    document.getElementById('second-sec'),
+  ],
+  [
+    document.getElementById('third-min'),
+    document.getElementById('third-sec'),
+  ],
+];
+
+// Minutes
+for (let j = 0; j < 3; j++) {
+  for (let i = 0; i < 61; i++) {
+    const option = document.createElement('option');
+    option.text = i;
+    option.value = i;
+    selectTags[j][0].appendChild(option);
+  }
+}
+
+// Seconds
+for (let j = 0; j < 3; j++) {
+  for (let i = 0; i < 60; i++) {
+    const option = document.createElement('option');
+    option.text = i;
+    option.value = i;
+    selectTags[j][1].appendChild(option);
+  }
+}
+
+B4_TIME_BUTTON.addEventListener('click', () => {
+  disabledTimeSelect(true);
+  selectTags[0][0].selectedIndex = 9;
+  selectTags[1][0].selectedIndex = 11;
+  selectTags[2][0].selectedIndex = 13;
+  for (let i = 0; i < 3; i++) {
+    selectTags[i][1].selectedIndex = 1;
+  }
+  START_STOP_BUTTON.disabled = false;
+  LOG_WINDOW.innerText = 'B4が選択されました．\n';
+});
+
+MX_TIME_BUTTON.addEventListener('click', () => {
+  disabledTimeSelect(true);
+  selectTags[0][0].selectedIndex = 10;
+  selectTags[1][0].selectedIndex = 12;
+  selectTags[2][0].selectedIndex = 14;
+  for (let i = 0; i < 3; i++) {
+    selectTags[i][1].selectedIndex = 1;
+  }
+  START_STOP_BUTTON.disabled = false;
+  LOG_WINDOW.innerText = 'Mxが選択されました．\n';
+});
+
+CUSTOM_TIME_BUTTON.addEventListener('click', () => {
+  disabledTimeSelect(false);
+  LOG_WINDOW.innerText = 'カスタムが選択されました．時間を指定してください．\n';
+});
+
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 2; j++) {
+    selectTags[i][j].addEventListener('click', ()=> {
+      if (isSelectedTime() === true) {
+        START_STOP_BUTTON.disabled = false;
+      }
+    });
+  }
+}
+
+/**
+ * Check the time is selected
+ *
+ * @return {boolean} time is selected
+ */
+function isSelectedTime() {
+  let countSelected = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 2; j++) {
+      if (selectTags[i][j].value === '0') {
+        countSelected += 1;
+      }
+    }
+  }
+
+  if (countSelected === 6) {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Make disable time-select
+ *
+ * @param {boolean} doDisabled
+ */
+function disabledTimeSelect(doDisabled) {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 2; j++) {
+      selectTags[i][j].disabled = doDisabled;
+    }
+  }
+}
--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,8 @@
 const TIMER_PARAGRAPH = document.getElementById('timer');
-const START_STOP_BUTTON = document.getElementById('start-stop-btn');
+// const START_STOP_BUTTON = document.getElementById('start-stop-btn');
 const RESET_BUTTON = document.getElementById('reset-btn');
 const SOUND_TEST_BUTTON = document.getElementById('sound-test-btn');
+// const LOG_WINDOW = document.getElementById('log-window');
 
 let time = 0;
 let runTimer = 0;
@@ -11,7 +12,7 @@ const AUDIO_FILE = new Audio('sound/Doorbell-Ding_Dong02-1.mp3');
 AUDIO_FILE.load();
 // When the audio file is loaded, start to play it.
 AUDIO_FILE.addEventListener('loadedmetadata', () => {
-  START_STOP_BUTTON.disabled = false;
+  START_STOP_BUTTON.textContent = 'Start';
 });
 // When the audio file could not be loaded, appear error message.
 AUDIO_FILE.addEventListener('error', () => {
@@ -24,28 +25,42 @@ START_STOP_BUTTON.addEventListener('click', () => {
     runTimer = 1;
     RESET_BUTTON.disabled = true;
     SOUND_TEST_BUTTON.disabled = true;
+    disabledTimeButton(true);
+    disabledTimeSelect(true);
+    const [SELECTED_SEC_LIST, MAX_SEC] = getSelectedSec();
+    console.log('SELECTED_SEC_LIST', SELECTED_SEC_LIST);
+    addLogMessage(time, '開始します．');
 
     timerId = setInterval(() => {
-      updateTimerParagraph(time);
       time += 1;
+      updateTimerParagraph(time);
+      if (isSelectedSec(SELECTED_SEC_LIST, time) === true) {
+        AUDIO_FILE.play();
+      }
+      if (MAX_SEC === time) {
+        SOUND_TEST_BUTTON.disabled = false;
+        stopTimer();
+        time = 0;
+        updateTimerParagraph(time);
+      }
     }, 1000);
   } else {
-    START_STOP_BUTTON.textContent = 'Start';
-    runTimer = 0;
-    RESET_BUTTON.disabled = false;
-    SOUND_TEST_BUTTON.disabled = false;
-
-    clearInterval(timerId);
+    SOUND_TEST_BUTTON.disabled = true;
+    stopTimer();
+    addLogMessage(time, '停止します．');
   }
 });
 
 RESET_BUTTON.addEventListener('click', () => {
   RESET_BUTTON.disabled = true;
-  SOUND_TEST_BUTTON.disabled = true;
+  SOUND_TEST_BUTTON.disabled = false;
+  disabledTimeButton(false);
+  disabledTimeSelect(false);
 
   time = 0;
   updateTimerParagraph(time);
   clearInterval(timerId);
+  addLogMessage(time, 'リセットします．');
 });
 
 SOUND_TEST_BUTTON.addEventListener('click', () => {
@@ -57,9 +72,90 @@ SOUND_TEST_BUTTON.addEventListener('click', () => {
  * @param {Integer} displayTime Current time
  */
 function updateTimerParagraph(displayTime) {
-  const min = Math.floor(displayTime / 60) % 60;
-  const sec = displayTime % 60;
+  const [min, sec] = sec2minsec(displayTime);
   console.log(`${min}:${sec}, displayTime: ${displayTime}`);
   TIMER_PARAGRAPH.textContent =
     `${min}:${('00' + sec).slice(-2)}`;
+}
+
+/**
+ * Convert seconds to minutes and seconds
+ *
+ * @param {Number} currentSec current seconds
+ * @return {List} Minutes and Seconds
+ */
+function sec2minsec(currentSec) {
+  const min = Math.floor(currentSec / 60) % 60;
+  const sec = currentSec % 60;
+  return [min, sec];
+}
+
+/**
+ * Stop timer
+ */
+function stopTimer() {
+  START_STOP_BUTTON.textContent = 'Start';
+  runTimer = 0;
+  RESET_BUTTON.disabled = false;
+
+  clearInterval(timerId);
+}
+
+/**
+ * Get selected time seconds
+ *
+ * @return {List} SEC_LIST, maxSec
+ */
+function getSelectedSec() {
+  const SEC_LIST = [0, 0, 0];
+  let maxSec = 0;
+  for (let i = 0; i < 3; i++) {
+    const MIN = Number(selectTags[i][0].value);
+    const SEC = Number(selectTags[i][1].value);
+    SEC_LIST[i] = MIN * 60 + SEC;
+    if (maxSec < SEC_LIST[i]) {
+      maxSec = SEC_LIST[i];
+    }
+  }
+  return [SEC_LIST, maxSec];
+}
+
+
+/**
+ * Detect the current time is selected time
+ *
+ * @param {List} selectedSec
+ * @param {Number} currentSec
+ * @return {boolean} is selected seconds
+ */
+function isSelectedSec(selectedSec, currentSec) {
+  for (let i = 0; i < 3; i++) {
+    if (selectedSec[i] == currentSec) {
+      addLogMessage(time, `${i+1}回目の合図です．`);
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * Make disable time-button
+ *
+ * @param {boolean} doDisabled
+ */
+function disabledTimeButton(doDisabled) {
+  B4_TIME_BUTTON.disabled = doDisabled;
+  MX_TIME_BUTTON.disabled = doDisabled;
+  CUSTOM_TIME_BUTTON.disabled = doDisabled;
+}
+
+/**
+ * Add log message to log window
+ *
+ * @param {Number} currentTime current time
+ * @param {String} message message
+ */
+function addLogMessage(currentTime, message) {
+  const [min, sec] = sec2minsec(currentTime);
+  LOG_WINDOW.innerText += `${min}:${('00' + sec).slice(-2)} ${message}\n`;
 }
